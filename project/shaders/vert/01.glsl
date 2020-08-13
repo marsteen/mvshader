dcc847d4a2c232af045ebc724962b885a850a22d
Martin Steen <martin.steen@ksti.de>
2020-08-13 13:49:19 +0200

    default shaders changed

---
+++ project/shaders/vert/01.glsl
@@ -0,0 +1,14 @@
+#version 300 es
+//
+// Vertex Shader
+//
+
+in vec4  aVertAttrib;
+in vec2  aTextAttrib;
+out vec2 vTextVary;
+
+void main()
+{
+    vTextVary = aTextAttrib;
+    gl_Position = aVertAttrib * 0.5f;
+}
