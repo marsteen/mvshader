cb8126fb9aed2eb7c0e513982a0e39a2911c09fa
Martin Steen <martin.steen@ksti.de>
2020-08-13 15:10:02 +0200

    shaders added

---
+++ project/shaders/frag/00-template.glsl
@@ -0,0 +1,34 @@
+#version 300 es
+//
+// Fragment Shader
+//
+#ifdef GL_ES
+precision highp float;
+#endif
+
+uniform vec3  iResolution;
+uniform vec4  iMouse;
+uniform float iTime;
+in vec2 vTextVary;
+out vec4 outputColor;
+
+// ********** REPLACE THIS PART WITH YOUR SHADER **********
+//
+//            START
+//
+//*********************************************************
+
+
+//*********************************************************
+//
+//            END
+//
+//*********************************************************
+
+void main()
+{
+    vec2 fragCoord = vTextVary * iResolution.xy;
+    mainImage(outputColor, fragCoord); 
+}
+
+
