cc38d99c051c77a9067c1ef6a84c972ed649aed7
Martin Steen <martin@martin-steen.de>
2020-09-16 10:09:36 +0200

    added script to install ubunut dev libs

--- git-push.sh
+++ git-push.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 #
+git config credential.helper store
 git push -u origin master
 
--- ubuntu-install-devlibs.sh
+++ ubuntu-install-devlibs.sh
@@ -2,4 +2,4 @@
 #
 # call with sudo to install development libs for this project
 #
-apt install libglew-dev libglfw3-dev
+apt install libglew-dev libglfw3-dev libsdl2-dev
